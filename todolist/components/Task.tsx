7f642fb6fd224ffde7ac9d5d50dc7d86e87de7d9
FRAZ5094 <frasergmorrison@btinternet.com>
2022-06-14 11:37:20 +0100

    done tutorial

--- todolist/components/Task.tsx
+++ todolist/components/Task.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const Task = (props : string) => {
+const Task = (props : { text: string } ) => {
 
   return (
 
     <View style={styles.item}>
       <View style={styles.itemLeft}>
-        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <View style={styles.square}></View>
         <Text style={styles.itemText}>{props.text}</Text>
       </View>
       <View style={styles.circular}></View>
@@ -23,6 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     padding: 15,
     borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
   },
   itemLeft: {
     flexDirection: 'row',
@@ -33,10 +37,20 @@ const styles = StyleSheet.create({
     width: 24,
     height: 24,
     backgroundColor: '#55BCF6',
-    opacity: 0.4
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
   },
-  itemText: {},
-  circular: {},
+  itemText: {
+    maxWidth: '80%',
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#55BCF6',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
 });
 
 export default Task;
